3e809fa8f1279edf19fe10176a8f9b2ca313ef46
Mari Kawakatsu <kawakatsu.mari@gmail.com>
2021-02-03 13:43:21 -0500

    updated test; updated dependencies; updated README

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CooperationPolarization
+
+Code for Kawakatsu, Lelkes, Levin, and Tarnita (2021) "Revisiting Madison’s Cure for the “Mischiefs of Faction”: Can Cooperation and Polarization Evolve Together?"
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,8 +4,8 @@ using Test
 @testset "CooperationPolarization.jl" begin
 
     # setup.jl: random_sets
-    @test random_sets(2, 1, 1, 2).p == [0, 1]
-    @test random_sets(8, 3, 2, 2).p == [0, 0, 0, 0, 1, 1, 1, 1]
+    @test random_sets(2, 1, 1, 2).p == [1, 2]
+    @test random_sets(8, 3, 2, 2).p == [1, 1, 1, 1, 2, 2, 2, 2]
     @test random_sets(8, 3, 2).p == random_sets(8, 3, 2, 2).p 
 
     # setup.jl: vectobase3
